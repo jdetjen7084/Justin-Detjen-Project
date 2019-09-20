e9c5206ff0e2fce7b9168c5b3e2c7e74a4de8e82
Justin Detjen <jdetjen@gmx.com>
2019-09-19 21:13:52 -0500

    Add greeter function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-const msg = prompt("What is your name?");
+//const msg = prompt("What is your name?");
 
-console.log(msg);
+function greeter(question = "What is your quest?") {
+  return prompt(question);
+}
+const answer = greeter();
+console.log(answer);
+//querySelector only selects first instance
+//document.querySelector('h1').textContent("Nope");
+
+//const h1TextContent = document.querySelector("h1").textContent;
+
+document.querySelector("h1").textContent = greeter();
+
+//console.log(h1TextContent);
