43f8bde12326770998fbe912aa254bfbf8936ed7
Justin Detjen <jdetjen@gmx.com>
2019-09-19 21:28:46 -0500

    Add greeter h1 functionality with name

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 //const msg = prompt("What is your name?");
-
+//If answer is blank, ask again:
 function greeter(question = "What is your quest?") {
-  return prompt(question);
+  let answer = prompt(question);
+  if (answer === ""){
+    greeter();
+  }
+  return answer;
 }
-const answer = greeter();
-console.log(answer);
+//TODO: Resolve issue where h1 is not updated if user gives blank answer
+//const answer = greeter();
+//console.log(answer);
 //querySelector only selects first instance
 //document.querySelector('h1').textContent("Nope");
 
@@ -12,4 +17,3 @@ console.log(answer);
 
 document.querySelector("h1").textContent = greeter();
 
-//console.log(h1TextContent);
