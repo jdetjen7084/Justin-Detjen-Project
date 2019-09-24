febd5dfe29fdd727cb169fa7433d710ea21c1490
Justin Detjen <jdetjen@gmx.com>
2019-09-23 21:29:01 -0500

    Fix listener glitch

--- scripts.js
+++ scripts.js
@@ -3,17 +3,12 @@
 function greeter(question = "What is your quest?") {
   let answer = prompt(question);
   if (answer === ""){
-    greeter();
+  return greeter();
   }
   return answer;
 }
-//TODO: Resolve issue where h1 is not updated if user gives blank answer
-//const answer = greeter();
-//console.log(answer);
-//querySelector only selects first instance
-//document.querySelector('h1').textContent("Nope");
 
-//const h1TextContent = document.querySelector("h1").textContent;
+//document.querySelector('h1').textContent("Nope");
 
 document.querySelector("h1").textContent = greeter();
 
