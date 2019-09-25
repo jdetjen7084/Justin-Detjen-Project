5d97ce58fac5825fef43a92a1db12dbbc171ae07
Justin Detjen <jdetjen@gmx.com>
2019-09-24 20:53:38 -0500

    Add skeleton for hamburger listener

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
-//const msg = prompt("What is your name?");
-//If answer is blank, ask again:
-function greeter(question = "What is your quest?") {
-  let answer = prompt(question);
-  if (answer === ""){
-  return greeter();
-  }
-  return answer;
-}
-//document.querySelector('h1').textContent("Nope");
-//innerHTML will enter HTML to doc, textContent will add content but won't insert into the HTML seamlessly
-//document.querySelector("#root").innerHTML = greeter();
-//Below is template literal
-document.querySelector("#root").innerHTML = `<p>Your quest is ${greeter}</p>`
+import { createPublicKey } from "crypto";
+
+// //const msg = prompt("What is your name?");
+// //If answer is blank, ask again:
+// function greeter(question = "What is your quest?") {
+//   let answer = prompt(question);
+//   if (answer === ""){
+//   return greeter();
+//   }
+//   return answer;
+// }
+// //document.querySelector('h1').textContent("Nope");
+// //innerHTML will enter HTML to doc, textContent will add content but won't insert into the HTML seamlessly
+// //document.querySelector("#root").innerHTML = greeter();
+// //Below is template literal
+// document.querySelector("#root").innerHTML = `<p>Your quest is ${greeter}</p>`
+
+//Below is JS for project
+//select fa-hamburger, listen for click on hamburger
+//remove 'mobile hidden' class
+const hamburger = document.querySelector('.fa-bars');
+const hiddenUL = document.querySelector("ul");
+
+hamburger.addEventListener('click', function() {
+
+} )
+
