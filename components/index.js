209374660a4b4cc16e2fb7b6ad09b56701c66cae
Justin Detjen <jdetjen@gmx.com>
2019-09-30 19:53:18 -0500

    Refector to use object destructuring

---
+++ components/index.js
@@ -0,0 +1,8 @@
+//Purpose of this file is to gather and re-export the components
+//The 'from' part (string name) refers to file names
+//The 'default as' is a variable name
+export {default as Header} from './Header';
+export {default as Nav} from './Nav';
+export {default as Main} from './Main';
+export {default as Footer} from './Footer';
+
