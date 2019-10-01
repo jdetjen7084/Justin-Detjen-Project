f589cb61a908f63c5a51d88d28642a78dd19246a
Justin Detjen <jdetjen@gmx.com>
2019-09-30 19:53:47 -0500

    Add object destructuring for index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-import Nav from "./components/Nav";
+//import Header from "./components/Header";
+//import Footer from "./components/Footer";
+//import Main from "./components/Main";
+//import Nav from "./components/Nav";
+
+import { Header, Nav, Main, Footer } from "./components";
 
 // Grab #root div to assign the markup that is
 // contained in the components
 document.querySelector("#root").innerHTML = `
-${Header()}
-${Nav()}
-${Main()}
-${Footer()}
+  ${Header()}
+  ${Nav()}
+  ${Main()}
+  ${Footer()}
 `;
 
+// Object Destructuring
+//const { name } = myObj;
+//console.log(name);
+
+//const {name, catchPhrase} = name;
+//is same as
+//const catchPhrase = myObj.catchPhrase;
 
