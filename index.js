9bb7c54fd440936849828f326cb5f56ebdef8070
Justin Detjen <jdetjen@gmx.com>
2019-10-02 11:24:07 -0500

    Finish headings for state, make CTA readable

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const state = {
     heading: 'About Page'
   },
   contact: {
-
+    heading: 'Contact Page'
+  },
+  gallery: {
+    heading: 'Gallery Page'
+  },
+  blog: {
+    heading: 'Blog Page'
   }
 }
 
@@ -42,14 +48,14 @@ const links = document.querySelectorAll('nav a');
 console.log(links);
 console.log(links[3]);
 //use for loop to log out the textContent property of each link
-for (let i = 1; i < 4; i += 1) {
+for (let i = 0; i < links.length; i += 1) {
   //const prop = links(i.textContent);
     links[i].addEventListener('click', function(event){
     event.preventDefault();
     //console.log(state[event.target.textContent]);
     //const aboutElement = event.target;
     //const aboutText = event.target.textContent;
-    console.log(state[event.target.textContent])
+    console.log(event.target.textContent)
     //render(state[event.target.textContent.toLowerCase()]);
   })
 }
